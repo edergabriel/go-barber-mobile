3a6c2513ef9a653837d95dc50b90a103b3cd0dc6
Eder Gabriel <eder1602@outlook.com>
2021-02-02 23:58:05 -0300

    Importando fontes externas

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { Image, Text, View } from 'react-native';
 
-import { Container } from './styles';
+import { Container, Title } from './styles';
 
 import logoImg from '../../assets/logo.png';
 
@@ -9,7 +9,10 @@ const SignIn: React.FC = () => {
     return (
         <Container>
             <Image source={logoImg} />
-        </Container>)
+
+            <Title>Faça seu login</Title>
+        </Container>
+    )
 
 }
 
